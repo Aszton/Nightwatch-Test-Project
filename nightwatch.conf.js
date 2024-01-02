cce089555914867aaf4972b3b5156b702b08f566
Aszton <bartoszkuczera88@gmail.com>
2024-01-02 17:14:50 +0100

    CI updates

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -4,7 +4,7 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['test','nightwatch'],
+  src_folders: ["test", "nightwatch"],
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
   page_objects_path: [],
@@ -17,65 +17,64 @@ module.exports = {
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   plugins: [],
-  
+
   // See https://nightwatchjs.org/guide/concepts/test-globals.html
-  globals_path: '',
-  
+  globals_path: "",
+
   webdriver: {},
 
   test_workers: {
-    enabled: true
+    enabled: true,
   },
 
   test_settings: {
     default: {
       disable_error_log: false,
-      launch_url: 'https://automationteststore.com',
+      launch_url: "https://automationteststore.com",
 
       screenshots: {
         enabled: false,
-        path: 'screens',
-        on_failure: true
+        path: "screens",
+        on_failure: true,
       },
 
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: "chrome",
       },
-      
+
       webdriver: {
         start_process: true,
-        server_path: ''
+        server_path: "",
       },
-      
     },
-    
+
     firefox: {
       desiredCapabilities: {
-        browserName: 'firefox',
+        browserName: "firefox",
         alwaysMatch: {
           acceptInsecureCerts: true,
-          'moz:firefoxOptions': {
+          "moz:firefoxOptions": {
             args: [
               // '-headless',
               // '-verbose'
-            ]
-          }
-        }
+            ],
+          },
+        },
       },
       webdriver: {
         start_process: true,
-        server_path: '',
+        server_path: "",
         cli_args: [
           // very verbose geckodriver logs
           // '-vv'
-        ]
-      }
+        ],
+      },
     },
-    
+
     chrome: {
       desiredCapabilities: {
-        browserName: 'chrome',
-        'goog:chromeOptions': {
+        browserName: "chrome",
+        "goog:chromeOptions": {
           // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
           //
           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
@@ -85,43 +84,39 @@ module.exports = {
             //'--ignore-certificate-errors',
             //'--allow-insecure-localhost',
             // '--headless'
-          ]
-        }
+          ],
+        },
       },
 
       webdriver: {
         start_process: true,
-        server_path: '/usr/bin/chromedriver',
+        server_path: "",
         port: 9515,
-        cli_args: [
-          // --verbose
-        ]
-      }
+        cli_args: ["--port=9515"],
+      },
     },
-    
+
     edge: {
       desiredCapabilities: {
-        browserName: 'MicrosoftEdge',
-        'ms:edgeOptions': {
+        browserName: "MicrosoftEdge",
+        "ms:edgeOptions": {
           w3c: true,
           // More info on EdgeDriver: https://docs.microsoft.com/en-us/microsoft-edge/webdriver-chromium/capabilities-edge-options
           args: [
             //'--headless'
-          ]
-        }
+          ],
+        },
       },
 
       webdriver: {
         start_process: true,
         // Follow https://docs.microsoft.com/en-us/microsoft-edge/webdriver-chromium/?tabs=c-sharp#download-microsoft-edge-webdriver
         // to download the Edge WebDriver and set the location of extracted `msedgedriver` below:
-        server_path: '',
+        server_path: "",
         cli_args: [
           // --verbose
-        ]
-      }
+        ],
+      },
     },
-    
   },
-  
 };
